c9e216a0fdac01603cd9ce1eb493cd4567ec198d
sonpham08 <gunmap0897@gmail.com>
2019-05-13 07:26:16 +0700

    Update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     var vocabularyList = [
-        {id:1,topic: "Công nghệ", img: "./image/congnghe.jpg", navigate: "./vocabulary_1_1.html"},
-        {id:2,topic: "Thể thao", img: "./image/thethao.jpg", navigate: "./vocabulary_2_1.html"},
-        {id:3, topic: "Động vật", img: "./image/dongvat.jpg", navigate: "./vocabulary_3_1.html"},
-        {id:4, topic: "Công việc", img: "./image/congviec.jpg", navigate: "./vocabulary_4_1.html"},
-        {id:5, topic: "Ẩm thực", img: "./image/amthuc.jpg", navigate: "./vocabulary_5_1.html"},
-        {id:6, topic: "Sức khỏe", img: "./image/suckhoe.jpg", navigate: "./vocabulary_6_1.html"},
-        {id:7,topic: "Kinh tế", img: "./image/kinhte.png", navigate: "./vocabulary_7_1.html"},
+        {id:1,topic: "Công nghệ", img: "./image/congnghe.jpg", navigate: "./vocabulary_1_1.html", exercise:"./vocabulary_ex_1_1.html"},
+        {id:2,topic: "Thể thao", img: "./image/thethao.jpg", navigate: "./vocabulary_2_1.html", exercise:"./vocabulary_ex_2_1.html"},
+        {id:3, topic: "Động vật", img: "./image/dongvat.jpg", navigate: "./vocabulary_3_1.html", exercise:"./vocabulary_ex_3_1.html"},
+        {id:4, topic: "Công việc", img: "./image/congviec.jpg", navigate: "./vocabulary_4_1.html", exercise:"./vocabulary_ex_4_1.html"},
+        {id:5, topic: "Ẩm thực", img: "./image/amthuc.jpg", navigate: "./vocabulary_5_1.html", exercise:"./vocabulary_ex_5_1.html"},
+        {id:6, topic: "Sức khỏe", img: "./image/suckhoe.jpg", navigate: "./vocabulary_6_1.html",exercise:"./vocabulary_ex_6_1.html"},
+        {id:7,topic: "Kinh tế", img: "./image/kinhte.png", navigate: "./vocabulary_7_1.html", exercise:"./vocabulary_ex_7_1.html"},
     ];
     localStorage.setItem('vocabulary', vocabularyList);
 
@@ -20,7 +20,10 @@ $(document).ready(function() {
 				                "<img src='"+vocabulary.img+"' height='200px' width='200px'  alt='Image'/>"+
 				            "</div>"+
 				            "<div>"+
-				                "<a href='"+vocabulary.navigate+"'><button type='button' class='btn btn-info'>Learn</button></a>"+
+                                "<a href='"+vocabulary.navigate+"'>"+
+                                "<button type='button' class='btn btn-info' style='margin-right: 5px'>Learn</button>"+
+                                "<a href='"+vocabulary.exercise+"'><button type='button' class='btn btn-success'>Exercise</button></a>"+
+                                "</a>"+
 				            "</div>"+
 				        "</div>"+
 				"</div>"+
@@ -37,7 +40,6 @@ function onLogin() {
     // e.preventDefault();
     let username = $("#username").val();
     let password = $("#password").val();
-    console.log(password);
     if(username == 'son' && password == '123') {
         window.open('./index.html');
         localStorage.setItem("user", JSON.stringify("son"));
